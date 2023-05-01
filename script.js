4eca39705972ef7f6420f7bcc881fd5719874b4a
Mehmud <111188985+Abdullah-Al-Mehmud@users.noreply.github.com>
2023-05-02 00:16:43 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+let username = document.getElementById("username");
+let password = document.getElementById("password");
+let flag = 1;
+
+
+function validateForm(){
+
+  if(username.value === ""){
+     document.getElementById("userError").innerHTML = "User name is empty";
+     flag = 0;
+  } else if(username.value.length < 3){
+    document.getElementById("userError").innerHTML = "User name require minimum 3 char";
+    flag = 0;
+  } else{
+    document.getElementById("userError").innerHTML = "";
+    flag = 1;
+  }
+
+  if(password.value === ""){
+      document.getElementById("passwordError").innerHTML = "Password required";
+      flag = 0;
+  } else{
+    document.getElementById("passwordError").innerHTML = "";
+    flag = 1;
+  }
+  
+  if(flag){
+    return true;
+  } else{
+    return false;
+  }
+}
+
+
+
